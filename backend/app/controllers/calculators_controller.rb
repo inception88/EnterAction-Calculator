88c2b2aa11cb657617c6527362c301408a8dddc6
inception88 <bljones36@gmail.com>
2020-05-29 13:51:25 -0400

    Added Ability to view all calculators in DB

--- backend/app/controllers/calculators_controller.rb
+++ backend/app/controllers/calculators_controller.rb
@@ -1,2 +1,6 @@
 class CalculatorsController < ApplicationController
+    def index
+        calculators = Calculator.all
+        render json: calculators, except: [:created_at, :updated_at]
+    end
 end
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,3 +4,16 @@ const BACKEND_URL = 'http://localhost:3000';
 fetch(`${BACKEND_URL}/test`)
   .then(response => response.json())
   .then(parsedResponse => console.log(parsedResponse));
+
+  fetch(`${BACKEND_URL}/calculators`)
+  .then(response => response.json())
+  .then(json => calculators(json));
+
+  function calculators(json) {
+    const main = document.getElementById('test')
+    json.forEach(calculator => {
+        const div = document.createElement('div');
+        div.innerHTML = `${calculator['name']}`;      
+        main.appendChild(div);
+      })
+  }
