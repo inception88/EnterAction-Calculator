b69f3c7b0a23e9317f50cbf313c3997e405005b0
inception88 <bljones36@gmail.com>
2020-05-29 12:49:20 -0400

    First commit

---
+++ frontend/src/index.js
@@ -0,0 +1,6 @@
+console.log("In dere!")
+
+const BACKEND_URL = 'http://localhost:3000';
+fetch(`${BACKEND_URL}/test`)
+  .then(response => response.json())
+  .then(parsedResponse => console.log(parsedResponse));
