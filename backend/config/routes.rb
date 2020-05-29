549126f1a4c7c971bf023ae3f6194770d4f0b346
inception88 <bljones36@gmail.com>
2020-05-29 13:18:15 -0400

    Added Calculator Resource

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+  resources :calculators
   get '/test', to: 'application#test'
 end
---
+++ backend/db/migrate/20200529171029_create_calculators.rb
@@ -0,0 +1,9 @@
+class CreateCalculators < ActiveRecord::Migration[5.2]
+  def change
+    create_table :calculators do |t|
+      t.string :name
+
+      t.timestamps
+    end
+  end
+end
