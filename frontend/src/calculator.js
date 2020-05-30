62b9986ea3887873d855b41beccb5b0a81f747ca
inception88 <bljones36@gmail.com>
2020-05-29 23:00:06 -0400

    Moved calculator functions to calculator.js

--- frontend/src/calculator.js
+++ frontend/src/calculator.js
@@ -1,3 +1,31 @@
 class Calculator {
-    
+    static allFetch() {
+        return fetch(`${BACKEND_URL}/calculators`)
+        .then(resp => resp.json())
+        .then(json => this.calculators(json)) 
+    }
+
+    static calculators(json) {
+        const main = document.getElementById('calc')
+        json.forEach(calculator => {
+            const opt = document.createElement('option');
+            opt.innerHTML = `${calculator['name']}`;
+            opt.id = `${calculator['id']}`      
+            main.appendChild(opt);
+        })
+    }
+
+    static current(json) {
+        const h1 = document.getElementById('current')
+        h1.innerHTML = json["name"];
+        const div = document.getElementById('info');
+        div.innerHTML = `Individual Goal: ${json["individualGoal"]}`+` Monthly Goal: ${json["monthlyGoal"]}`
+    }
+
+    static select(dropDown) {
+        const optionId = dropDown.options[dropDown.selectedIndex].id
+        return fetch(`${BACKEND_URL}/calculators/${optionId}`)
+        .then(resp => resp.json())
+        .then(json => this.current(json))
+    }
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,41 +3,11 @@ const BACKEND_URL = 'http://localhost:3000';
 document.addEventListener("DOMContentLoaded", () => {
         
         const dropDown = document.getElementById('calc')
-        dropDown.addEventListener("change", function() {select(dropDown)});
+        dropDown.addEventListener("change", function() {Calculator.select(dropDown)});
         
         async function f() {
-            await fetchCalculators();
-            select(dropDown);
+            await Calculator.allFetch();
+            Calculator.select(dropDown);
         }
         f();
 })
-
-function fetchCalculators() {
-    return fetch(`${BACKEND_URL}/calculators`)
-    .then(resp => resp.json())
-    .then(json => calculators(json)) 
-}
-
-function calculators(json) {
-    const main = document.getElementById('calc')
-    json.forEach(calculator => {
-        const opt = document.createElement('option');
-        opt.innerHTML = `${calculator['name']}`;
-        opt.id = `${calculator['id']}`      
-        main.appendChild(opt);
-    })
-}
-
-function calculator(json) {
-    const h1 = document.getElementById('current')
-    h1.innerHTML = json["name"];
-    const div = document.getElementById('info');
-    div.innerHTML = `Individual Goal: ${json["individualGoal"]}`+` Monthly Goal: ${json["monthlyGoal"]}`
-}
-
-function select(dropDown) {
-    optionId = dropDown.options[dropDown.selectedIndex].id
-    return fetch(`${BACKEND_URL}/calculators/${optionId}`)
-    .then(resp => resp.json())
-    .then(json => calculator(json))
-}
