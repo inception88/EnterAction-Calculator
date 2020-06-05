3ee3ce7bfa11bef9fcf037991e6df47eb10823c3
inception88 <bljones36@gmail.com>
2020-06-04 21:06:17 -0400

    Removed validation that percents must be integer

--- backend/app/models/product.rb
+++ backend/app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ApplicationRecord
     belongs_to :calculator
-    validates :netPercentage, numericality: { less_than: 100,  greater_than: 0, only_integer: true }
-    validates :commission, numericality: { less_than: 100,  greater_than_or_equal_to: 0, only_integer: true }
+    validates :netPercentage, numericality: { less_than: 100,  greater_than: 0 }
+    validates :commission, numericality: { less_than: 100,  greater_than_or_equal_to: 0 }
     validates :cost, presence: true
     validates :name, presence: true
 
